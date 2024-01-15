3fad0feb8efca076ff376b0b6ca8fdc712187e65
Michael Rodler <m@mrodler.eu>
2024-01-15 13:08:42 +0100

    enable env_logger in cli tool

--- src/cli.rs
+++ src/cli.rs
@@ -1,31 +1,33 @@
 use fzero_gen::*;
 
 fn main() -> std::io::Result<()> {
+    env_logger::init();
+
     // Get access to the command line arguments
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 4 {
-        println!("usage: fzero <grammar json> <output Rust file> <default max depth>");
+        log::warn!("usage: fzero <grammar json> <output Rust file> <default max depth>");
         return Ok(());
     }
 
     // Load up a grammar file
     let grammar: Grammar = serde_json::from_slice(&std::fs::read(&args[1])?)?;
-    println!("Loaded grammar json; parsing grammar into in-memory format.");
+    log::info!("Loaded grammar json; parsing grammar into in-memory format.");
 
     // Convert the grammar file to the Rust structures
     let mut gram = GrammarRust::new(&grammar, None);
-    println!("Converted grammar to in-memory format; optimizing now.");
+    log::info!("Converted grammar to in-memory format; optimizing now.");
 
     // Optimize the grammar
     gram.optimize();
-    println!("Optimized grammar; generating code.");
+    log::info!("Optimized grammar; generating code.");
 
     // Generate a Rust application
     gram.program(
         &args[2],
         args[3].parse().expect("Invalid digit in max depth"),
     );
-    println!("Generated Rust source file");
+    log::info!("Generated Rust source file");
 
     Ok(())
 }
